f78b094b50d8450ab28a178450b2d0b5d75f4197
halvorsen <aaron.halvorsen@gmail.com>
2018-09-29 16:52:17 -0400

    added alert for new version2

--- firetail/DashboardViewController.swift
+++ firetail/DashboardViewController.swift
@@ -614,15 +614,13 @@ class DashboardViewController: ViewSetup, UITextFieldDelegate, UIScrollViewDeleg
         if !AppStore.shared.askedToUpdateRecently() && !AppStore.shared.isVersionCurrent {
             let alert = UIAlertController(title: "New Version", message: "App Update Available", preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: { _ in
-                if let firetailURL = URL(string: "itms-apps//itunes.apple.com/app/id1210030205") {
+                if let firetailURL = URL(string: "itms-apps//itunes.apple.com/app/id1210030205"),
                     UIApplication.shared.canOpenURL(firetailURL) {
-                        UIApplication.shared.open(firetailURL)
-                    }
+                    UIApplication.shared.open(firetailURL)
                 }
             }))
-            alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
-        
-            present(alert, animated: true, completion: nil)
+                alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
+                present(alert, animated: true, completion: nil)
         }
     }
     
